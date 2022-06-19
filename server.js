b8aa8e06b15ea3f489de79e753c278dbd2f1fd13
IlhamPratama1 <pratamailham206@gmail.com>
2022-06-19 08:54:17 +0000

    current prod

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require("./app/models/index");
 dotenv.config();
 
 // CORS
-var corsOptions = { origin: "https://interactive-linked-list-app.netlify.app/" };
+var corsOptions = { origin: "https://interactive-linked-list-app.netlify.app" };
 
 // #region Middleware
 app.use(cors(corsOptions));
